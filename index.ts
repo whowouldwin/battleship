078120a24211c261aba6aa348d746fce9d08ee42
whowouldwin <leranetwork@gmail.com>
2024-10-25 21:43:07 +0300

    feat: add ESLINT and prettier

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import {httpServer} from "./src/http_server/index.ts";
-import {WebSocketServer} from 'ws'
+import { httpServer } from './src/http_server/index.ts';
+import { WebSocketServer } from 'ws';
 
 const HTTP_PORT = 8181;
 
@@ -11,7 +11,7 @@ wss.on('connection', (ws) => {
   console.log(`New connection connected: ${ws}`);
   ws.on('message', (message) => {
     console.log(`Received message: ${message.toString()}`);
-  })
+  });
   ws.on('close', () => {
     console.log(`Client disconnected: ${ws}`);
   });
